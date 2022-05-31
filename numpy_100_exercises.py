edbb92236a8f1f7281f841f755a4deeac3dff706
Sharon Lee <dasom.s.lee@gmail.com>
2022-06-01 00:12:04 +0900

    initial

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -300,3 +300,8 @@ np.einsum("ij,ji->i", A, B)
 
 # Consider the vector [1, 2, 3, 4, 5], how to build a new vector
 with 3 consecutive zeros interleaved between each value?
+Z = np.array([1,2,3,4,5])
+nz = 3
+Z0 = np.zeros(len(Z) + (len(Z)-1)*(nz))
+Z0[::nz+1] = Z
+print(Z0)
