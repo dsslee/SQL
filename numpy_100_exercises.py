23ded4aa4f21d45998b41a85cd702b30f58926ad
Sharon Lee <dasom.s.lee@gmail.com>
2022-05-31 19:07:46 +0900

    initial

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -75,3 +75,6 @@ print(Z)
 # Multiply a 5x3 matrix by a 3x2 matrix (real matrix product)
 Z = np.dot(np.ones((5,3)), np.ones((3,2)))
 print(Z)
+
+# Given a 1D array, negate all elements which are between 3
+and 8, in place.
