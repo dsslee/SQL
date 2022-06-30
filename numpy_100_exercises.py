b77fb8d9a635c09b2380e5dc5c3f0a4cc2ae294c
Sharon Lee <dasom.s.lee@gmail.com>
2022-06-30 22:16:51 +0900

    initial

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -173,3 +173,10 @@ print(np.linalg.det(C))
 
 # Print the minimum and maximum representable value for
 each numpy scalar type
+for dtype in [np.int8, np.int32, np.int64]:
+print(np.iinfo(dtype).min)
+print(np.iinfo(dtype).max)
+for dtype in [np.float32, np.float64]:
+print(np.finfo(dtype).min)
+print(np.finfo(dtype).max)
+print(np.finfo(dtype).eps)
