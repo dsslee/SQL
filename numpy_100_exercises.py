fe9e47b5470221adc2b19d08f796c04a07925af0
Sharon Lee <dasom.s.lee@gmail.com>
2022-07-06 06:19:06 +0900

    initial

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -183,3 +183,10 @@ print(np.finfo(dtype).eps)
 
 
 # Generate a generic 2D Gaussian-like array
+X, Y = np.meshgrid(np.linspace(-1,1,10), np.linspace(-1,1,10))
+D = np.sqrt(X*X+Y*Y)
+sigma, mu = 1.0, 0.0
+G = np.exp(-((D-mu)**2 / ( 2.0 * sigma**2 ) ) )
+print(G)
+
+# new
