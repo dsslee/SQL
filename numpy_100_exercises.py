171dbe8aabc2457f852bbaf61051f82dec8adb66
Sharon Lee <dasom.s.lee@gmail.com>
2022-07-18 06:51:14 +0900

    initial

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -30,31 +30,31 @@ print(Z)
 nz = np.nonzero([1,2,0,0,4,0])
 print(nZ)
 
-#Q10) Create a 3x3 identity matrix 
+#Q10) Create a 3x3 identity matrix.
 Z = np.eye(3)
 print(Z)
 
-# Create a 3x3x3 array with random values
+# Create a 3x3x3 array with random values.
 Z = np.randm.random((3,3,3))
 print(Z)
 
 # Create a 10x10 array with random values and find the
-minimum and maximum values
+minimum and maximum values.
 Z = np.random.random((10,10))
 Zmin, Zmax = Z.min(), Z.max()
 print(Zmin, Zmax)
 
-# Create a random vector of size 30 and find the mean value
+# Create a random vector of size 30 and find the mean value.
 Z = np.random.random(30)
 m = Z.mean()
 print(m)
 
-# Create a 2d array with 1 on the border and 0 inside
+# Create a 2d array with 1 on the border and 0 inside.
 Z = np.ones(10,10)
 Z[1:-1, 1:-1] = 0
 
 # Create a 5x5 matrix with values 1,2,3,4 just below the
-diagonal
+diagonal.
 Z = np.diag(1+np.arange(4),k=-1)
 print(Z)
 
