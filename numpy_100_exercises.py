d5cd38a24d10a7da0a6124ff52c776a2dc9346c9
Sharon Lee <dasom.s.lee@gmail.com>
2022-09-06 00:17:11 +0900

    Q64

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -316,3 +316,10 @@ A[[0,1]] = A[[1,0]]
 print(A)
 
 # Consider a set of 10 triplets describing 10 triangles (with shared vertices), find the set of unique line segments composing all the triangles.
+faces = np.random.randint(0,100,(10,3))
+F = np.roll(faces.repeat(2,axis=1),-1,axis=1)
+F = F.reshape(len(F)*3,2)
+F = np.sort(F,axis=1)
+G = F.view( dtype=[('p0',F.dtype),('p1',F.dtype)] )
+G = np.unique(G)
+print(G)
