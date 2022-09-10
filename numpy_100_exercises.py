c0d4c91bffaa8f931aa0500094d0bf1b1002064f
Sharon Lee <dasom.s.lee@gmail.com>
2022-09-10 20:41:35 +0900

    Q64

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -331,3 +331,9 @@ print(A)
 
 # How to compute averages using a sliding window over an
 array?
+def moving_average(a, n=3) :
+ret = np.cumsum(a, dtype=float)
+ret[n:] = ret[n:] - ret[:-n]
+return ret[n - 1:] / n
+Z = np.arange(20)
+print(moving_average(Z, n=3))
