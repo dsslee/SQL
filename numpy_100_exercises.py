981a594a6a5eb63aae6149d87ab606ac33afcb97
Sharon Lee <dasom.s.lee@gmail.com>
2022-09-14 20:45:13 +0900

    Q64

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -350,3 +350,16 @@ Z = np.random.randint(0,2,100)
 np.logical_not(arr, out=arr)
 Z = np.random.uniform(-1.0,1.0,100)
 np.negative(arr, out=arr)
+
+# Consider 2 sets of points P0,P1 describing lines (2d) and a point p, how to compute distance from p to each line i (P0[i],P1[i])?
+def distance(P0, P1, p):
+T = P1 - P0
+L = (T**2).sum(axis=1)
+U = -((P0[:,0]-p[...,0])*T[:,0] + (P0[:,1]-p[...,1])*T[:,1]) / L
+U = U.reshape(len(U),1)
+D = P0 + U*T - p
+return np.sqrt((D**2).sum(axis=1))
+P0 = np.random.uniform(-10,10,(10,2))
+P1 = np.random.uniform(-10,10,(10,2))
+p = np.random.uniform(-10,10,(1,2))
+print(distance(P0, P1, p))
