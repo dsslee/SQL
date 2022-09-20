4548a7cb266f8f9da671290326f8ea7e3af58cbf
Sharon Lee <dasom.s.lee@gmail.com>
2022-09-20 15:30:24 +0900

    Q72

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -370,4 +370,4 @@ P1 = np.random.uniform(-10,10,(10,2))
 p = np.random.uniform(-10, 10, (10,2))
 print np.array([distance(P0,P1,p_i) for p_i in p])
 
-# Consider an arbitrary array, write a function that extract a subpart with a fixed shape and centered on a given element.
+# Consider an array Z = [1,2,3,4,5,6,7,8,9,10,11,12,13,14], how to generate an array R = [[1,2,3,4], [2,3,4,5], [3,4,5,6], ..., [11,12,13,14]]?
