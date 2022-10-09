e2c3fab1915d6eb7408a129007820d0bb11c3493
Sharon Lee <dasom.s.lee@gmail.com>
2022-10-09 15:39:39 +0900

    Q86

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -426,3 +426,6 @@ print(U)
 I = np.array([0, 1, 2, 3, 15, 16, 32, 64, 128])
 B = ((I.reshape(-1,1) & (2**np.arange(8))) != 0).astype(int)
 print(B[:,::-1])
+
+I = np.array([0, 1, 2, 3, 15, 16, 32, 64, 128], dtype=np.uint8)
+print(np.unpackbits(I[:, np.newaxis], axis=1))
