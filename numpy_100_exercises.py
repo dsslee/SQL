7a5af9ed62895df2c3d9dbd4aa6099c51e8fc5a1
Sharon Lee <dasom.s.lee@gmail.com>
2022-10-10 15:41:04 +0900

    Q87

--- numpy_100_exercises.py
+++ numpy_100_exercises.py
@@ -435,4 +435,3 @@ Z = np.random.randint(0,2,(6,3))
 T = np.ascontiguousarray(Z).view(np.dtype((np.void, Z.dtype.itemsize * Z.shape[1])))
 _, idx = np.unique(T, return_index=True)
 uZ = Z[idx]
-print(uZ)
